fa8baa77b87fc7d9ae18ef620486e103f3a04d69
lmegviar <lmegviar@gmail.com>
2017-03-14 13:13:33 -0700

    Compute and display phi coefficients

--- server/server-controller.js
+++ server/server-controller.js
@@ -10,6 +10,14 @@ exports.getStats = function(req, res) {
   })
 };
 
+var findPhi = function(food) {
+  return (food['10'] * food['0'] - food['10'] * food['1']) /
+    Math.sqrt((food['10'] + food['10']) *
+              (food['0'] + food['1']) *
+              (food['1'] + food['10']) *
+              (food['0'] + food['10']));
+};
+
 exports.putLog = function(req, res) {
   var foodA = req.body.foodAcheck;
   var foodB = req.body.foodBcheck;
@@ -25,20 +33,37 @@ exports.putLog = function(req, res) {
         console.error(err)
       } else {
           if(foodA) {
-            found.foods[0]['0'] += 1;
+            found.foods[0]['11'] += 1;
+          } else {
+            found.foods[0]['10'] += 1;
           }
           if(foodB) {
-            found.foods[1]['0'] += 1;
+            found.foods[1]['11'] += 1;
+          } else {
+            found.foods[1]['10'] += 1;
           }
           if(foodC) {
-            found.foods[2]['0'] += 1;
+            found.foods[2]['11'] += 1;
+          } else {
+            found.foods[2]['10'] += 1;
           }
           if(foodD) {
-            found.foods[3]['0'] += 1;
+            found.foods[3]['11'] += 1;
+          } else {
+            found.foods[3]['10'] += 1;
           }
           if(foodE) {
-            found.foods[4]['0'] += 1;
+            found.foods[4]['11'] += 1;
+          } else {
+            found.foods[4]['10'] += 1;
           }
+
+          found.foods[0].lastPhi = findPhi(found.foods[0]);
+          found.foods[1].lastPhi = findPhi(found.foods[1]);
+          found.foods[2].lastPhi = findPhi(found.foods[2]);
+          found.foods[3].lastPhi = findPhi(found.foods[3]);
+          found.foods[4].lastPhi = findPhi(found.foods[4]);
+
           found.save(function (err){
             if (err) {
               console.error(err);
@@ -47,16 +72,57 @@ exports.putLog = function(req, res) {
         }
       });
     }
+
+  if (!symptoms) {
+    console.log('symptoms put log!')
+    Record.findOne({username: 'demoUser'}, function(err, found) {
+      if (err) {
+        console.error(err)
+      } else {
+          if(foodA) {
+            found.foods[0]['1'] += 1;
+          } else {
+            found.foods[0]['0'] += 1;
+          }
+          if(foodB) {
+            found.foods[1]['1'] += 1;
+          } else {
+            found.foods[1]['0'] += 1;
+          }
+          if(foodC) {
+            found.foods[2]['1'] += 1;
+          } else {
+            found.foods[2]['0'] += 1;
+          }
+          if(foodD) {
+            found.foods[3]['1'] += 1;
+          } else {
+            found.foods[3]['0'] += 1;
+          }
+          if(foodE) {
+            found.foods[4]['1'] += 1;
+          } else {
+            found.foods[4]['0'] += 1;
+          }
+
+          found.foods[0].lastPhi = findPhi(found.foods[0]);
+          found.foods[1].lastPhi = findPhi(found.foods[1]);
+          found.foods[2].lastPhi = findPhi(found.foods[2]);
+          found.foods[3].lastPhi = findPhi(found.foods[3]);
+          found.foods[4].lastPhi = findPhi(found.foods[4]);
+
+          found.save(function (err){
+            if (err) {
+              console.error(err);
+            }
+          })
+        }
+      });
+    }
+
     res.send();
   };
 
 
 
-  // var findPhi = function(food) {
-  //   return (food['10'] * food['00'] - food['10'] * food['01']) /
-  //     Math.sqrt((food['10'] + food['10']) *
-  //               (food['00'] + food['01']) *
-  //               (food['01'] + food['10']) *
-  //               (food['00'] + food['10']));
-  // };
 
